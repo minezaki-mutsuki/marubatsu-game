fcf4bf2f3a58850df7bd89da18e360ed19f1d3e3
Ravie403 <info@tkm.blue>
2022-04-28 13:42:31 +0900

    initial

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import './App.css';
+const App = () => {
+  return (
+    <div className="App">
+      <header className="App-header">React Workshop</header>
+      <main className="App-main">
+        <div id="board">
+          <div className="tile">1</div>
+          <div className="tile">2</div>
+          <div className="tile">3</div>
+          <div className="tile">4</div>
+          <div className="tile">5</div>
+          <div className="tile">6</div>
+          <div className="tile">7</div>
+          <div className="tile">8</div>
+          <div className="tile">9</div>
+        </div>
+      </main>
+    </div>
+  );
+}
+
+export default App;
