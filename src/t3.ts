bcd1168c609400534e697f9794c84e201295c645
Ravie403 <Ravie403@users.noreply.github.com>
2022-04-28 05:12:34 +0000

    deploy: a82a1825088502e6e94282c86d12317600441f9c

--- src/t3.ts
+++
@@ -1,9 +0,0 @@
-export const tileFromState = (state: boolean | null): string => state === null ? '' : state ? 'o' : 'x';
-export const judgeBoard = (board: readonly (string | null)[]): string | null => {
-  const JUDGE_BOARD_PRESET = [
-    [0, 1, 2], [3, 4, 5], [6, 7, 8],
-    [0, 3, 6], [1, 4, 7], [2, 5, 8],
-    [0, 4, 8], [2, 4, 6]
-  ];
-  return ['o', 'x'].find(p => JUDGE_BOARD_PRESET.some(ns => ns.every(b => board[b] === p))) ?? null;
-};
